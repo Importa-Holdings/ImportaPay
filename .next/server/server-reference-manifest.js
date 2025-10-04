f244b186be478718768c1693d78468d93e25932d
techwithgwin <godwinfriday31@gmail.com>
2025-10-04 13:16:09 +0100

    resolving error

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"h0ku54Nv7j2VWOrCnpV5Nyp5TctO6j+WezdG2J8IUuk=\"\n}"
