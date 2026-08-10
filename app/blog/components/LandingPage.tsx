90a9a67acd653366bf2f7c7f092b9aac94310893
Bakpo Godwin <67370368+Hood-Codivo@users.noreply.github.com>
2026-08-10 21:39:30 +0100

    new update

--- app/blog/components/LandingPage.tsx
+++ app/blog/components/LandingPage.tsx
@@ -4,10 +4,10 @@ import Subscribe from "./Subscribe";
 
 const LandingPage = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-white">
+    <div className="flex flex-col bg-white">
       {/* Main Content */}
-      <main className="flex-1 flex flex-col">
-        <div className="flex-1 flex items-center">
+      <main className="flex flex-col">
+        <div className="flex items-center">
           <div className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-16">
             <div className="max-w-4xl space-y-7">
               {/* Hero Section */}
